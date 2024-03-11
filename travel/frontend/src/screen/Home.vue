bdafe4a0ff15d4d4de85bcd8104c89014a7f4c0c
Rerksurat <64070094@kmitl.ac.th>
2024-03-11 21:04:49 +0700

    update home

--- travel/frontend/src/screen/Home.vue
+++ travel/frontend/src/screen/Home.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import {
   Dialog,
   DialogPanel,
@@ -21,14 +21,10 @@ import {
   XMarkIcon,
 } from '@heroicons/vue/24/outline'
 import { ChevronDownIcon, PhoneIcon, PlayCircleIcon, UserCircleIcon, CheckIcon } from '@heroicons/vue/20/solid'
+import Carousel from 'primevue/carousel';
+import Tag from 'primevue/tag';
+import Button from 'primevue/button';
 
-
-const includedFeatures = [
-  'Private forum access',
-  'Member resources',
-  'Entry to annual conference',
-  'Official member t-shirt',
-]
 
 const products = [
   { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
@@ -140,7 +136,7 @@ const mobileMenuOpen = ref(false)
         <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Company</a>
       </PopoverGroup> -->
         <div class=" lg:flex lg:flex-1 lg:justify-end">
-          <UserCircleIcon class="h-10 w-10" />
+          <UserCircleIcon class="h-10 w-10 text-white" />
         </div>
       </nav>
 
@@ -205,110 +201,84 @@ const mobileMenuOpen = ref(false)
         </div>
       </div>
 
-      <!-- <div class="bg-white rounded-2xl mt-6 "> -->
-      <!-- <div class="mx-auto max-w-2xl px-4 py-4 "> -->
-
-      <div v-for="product in products2" :key="product.id" class="group relative">
-        <div class="bg-white rounded-2xl p-4 my-4 grid grid-cols-1 gap-x-6 gap-y-4">
-          <div
-            class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-            <img :src="product.imageSrc" :alt="product.imageAlt"
-              class="h-full w-full object-cover object-center lg:h-full lg:w-full rounded-2xl" />
-          </div>
-          <h2 class=" text-2xl font-bold tracking-tight text-gray-900">เที่ยว</h2>
-          <div class="mt-1 flex justify-between">
-            <div>
-              <h3 class="text-sm text-gray-700">
-                <a :href="product.href">
-                  <span aria-hidden="true" class="absolute inset-0" />
-                  {{ product.name }}
-                </a>
-              </h3>
-              <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
+      <div>
+        <Carousel :value="places" :numVisible="1" :numScroll="1">
+          <template #item="slotProps">
+            <div class="border border-surface-200 dark:border-surface-700 rounded-md m-2 p-3 bg-lightgreen">
+              <div class="mb-3">
+                <div class="relative mx-auto">
+                  <img :src="slotProps.data.img" class="w-full rounded-md" />
+                  <Tag class="absolute" style="left:5px; top: 5px" />
+                </div>
+              </div>
+              <div class="mb-3 font-medium">{{slotProps.data.province}}</div>
+              <div class="flex justify-between items-center">
+                <div class="mt-0 font-semibold text-xl">{{slotProps.data.name}}</div>
+              </div>
             </div>
-            <p class="text-sm font-medium text-gray-900">{{ product.price }}</p>
-          </div>
-        </div>
+          </template>
+        </Carousel>
       </div>
-      <!-- </div> -->
-      <!-- </div> -->
 
-      <div class="text-xl font-medium">
+      <div class="text-2xl font-medium">
         Top Place
       </div>
-      <div v-for="product in products2" :key="product.id" class="group relative">
-        <div class="bg-white rounded-2xl p-4 my-4 grid grid-cols-1 gap-x-6 gap-y-4 ">
+      <div v-for="product in places" :key="product.name" class="group relative">
+        <div class="bg-lightgreen rounded-2xl p-4 my-4 grid grid-cols-1 gap-x-6 gap-y-4 ">
           <div
-            class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-            <img :src="product.imageSrc" :alt="product.imageAlt"
+            class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md lg:aspect-none group-hover:opacity-75 lg:h-80">
+            <img :src="product.img"
               class="h-full w-full object-cover object-center lg:h-full lg:w-full rounded-2xl" />
           </div>
-          <h2 class=" text-2xl font-bold tracking-tight text-gray-900">เที่ยว</h2>
+          <h2 class=" text-2xl font-bold tracking-tight text-gray-900">{{product.name}}</h2>
           <div class="mt-1 flex justify-between">
             <div>
               <h3 class="text-sm text-gray-700">
-                <a :href="product.href">
+                <a :href="product.name">
                   <span aria-hidden="true" class="absolute inset-0" />
-                  {{ product.name }}
+                  {{ product.province }}
                 </a>
               </h3>
-              <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
+              <p class="mt-1 text-sm text-gray-500">{{ product.description }}</p>
             </div>
-            <p class="text-sm font-medium text-gray-900">{{ product.price }}</p>
+            <p class="text-sm font-medium text-gray-900">{{ product.price }} ฿</p>
           </div>
         </div>
       </div>
     </div>
-    <!-- <div class="bg-white py-24 sm:py-32">
-      <div class="mx-auto max-w-7xl px-6 lg:px-8">
-        <div class="mx-auto max-w-2xl sm:text-center">
-          <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Simple no-tricks pricing</h2>
-          <p class="mt-6 text-lg leading-8 text-gray-600">Distinctio et nulla eum soluta et neque labore quibusdam.
-            Saepe et quasi iusto modi velit ut non voluptas in. Explicabo id ut laborum.</p>
-        </div>
-        <div class="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
-          <div class="p-8 sm:p-10 lg:flex-auto">
-            <h3 class="text-2xl font-bold tracking-tight text-gray-900">Lifetime membership</h3>
-            <p class="mt-6 text-base leading-7 text-gray-600">Lorem ipsum dolor sit amet consect etur adipisicing elit.
-              Itaque amet indis perferendis blanditiis repellendus etur quidem assumenda.</p>
-            <div class="mt-10 flex items-center gap-x-4">
-              <h4 class="flex-none text-sm font-semibold leading-6 text-indigo-600">What’s included</h4>
-              <div class="h-px flex-auto bg-gray-100" />
-            </div>
-            <ul role="list" class="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6">
-              <li v-for="feature in includedFeatures" :key="feature" class="flex gap-x-3">
-                <CheckIcon class="h-6 w-5 flex-none text-indigo-600" aria-hidden="true" />
-                {{ feature }}
-              </li>
-            </ul>
-          </div>
-          <div class="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
-            <div
-              class="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
-              <div class="mx-auto max-w-xs px-8">
-                <p class="text-base font-semibold text-gray-600">Pay once, own it forever</p>
-                <p class="mt-6 flex items-baseline justify-center gap-x-2">
-                  <span class="text-5xl font-bold tracking-tight text-gray-900">$349</span>
-                  <span class="text-sm font-semibold leading-6 tracking-wide text-gray-600">USD</span>
-                </p>
-                <a href="#"
-                  class="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get
-                  access</a>
-                <p class="mt-6 text-xs leading-5 text-gray-600">Invoices and receipts available for easy company
-                  reimbursement</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> -->
-    <!-- <div> -->
+
 
 
   </div>
 </template>
 
+<script>
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      places: []
+    };
+  },
+
+  created() {
+    this.fetchPlaces();
+  },
+  methods: {
+    async fetchPlaces() {
+      console.log("fetchplaces")
+      try {
+        const response = await axios.get('http://localhost:4000/places');
+        this.places = response.data; // Store the retrieved comments data in the 'comments' array
+        console.log(this.places)
+      } catch (error) {
+        console.error('Error fetching comments:', error);
+      }
+    }
+  }
+};
+</script>
 <style scoped>
 /* header {
   line-height: 1.5;
